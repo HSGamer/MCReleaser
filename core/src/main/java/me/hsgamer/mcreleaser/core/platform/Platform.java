10d33e699851b02ad2901110c1b405b62864edc9
HSGamer <huynhqtienvtag@gmail.com>
2024-02-13 20:27:56 +0700

    make the runnable optional

--- core/src/main/java/me/hsgamer/mcreleaser/core/platform/Platform.java
+++ core/src/main/java/me/hsgamer/mcreleaser/core/platform/Platform.java
@@ -3,6 +3,8 @@ package me.hsgamer.mcreleaser.core.platform;
 import me.hsgamer.hscore.task.BatchRunnable;
 import me.hsgamer.mcreleaser.core.file.FileBundle;
 
+import java.util.Optional;
+
 public interface Platform {
-    BatchRunnable createUploadTask(FileBundle fileBundle);
+    Optional<BatchRunnable> createUploadRunnable(FileBundle fileBundle);
 }
