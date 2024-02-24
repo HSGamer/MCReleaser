a9d8a262f7ba0dc26a3bc702c376b1c0425c0322
HSGamer <huynhqtienvtag@gmail.com>
2024-02-25 00:39:42 +0700

    temporarily use Text Renderer for description in Polymart

--- polymart/src/main/java/me/hsgamer/mcreleaser/polymart/PolymartPlatform.java
+++ polymart/src/main/java/me/hsgamer/mcreleaser/polymart/PolymartPlatform.java
@@ -6,7 +6,7 @@ import me.hsgamer.mcreleaser.core.file.FileBundle;
 import me.hsgamer.mcreleaser.core.platform.Platform;
 import me.hsgamer.mcreleaser.core.property.CommonPropertyKey;
 import me.hsgamer.mcreleaser.core.util.PropertyKeyUtil;
-import me.hsgamer.mcreleaser.html.MarkdownToHTMLConverter;
+import me.hsgamer.mcreleaser.renderer.text.MarkdownToTextConverter;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
@@ -48,8 +48,8 @@ public class PolymartPlatform implements Platform {
 
             try {
                 String description = CommonPropertyKey.DESCRIPTION.getValue();
-                String htmlDescription = MarkdownToHTMLConverter.convert(description);
-                builder.addTextBody("message", htmlDescription);
+                String textDescription = MarkdownToTextConverter.convert(description); // TODO: Convert to BBCode or ask Polymart to support Markdown or HTML
+                builder.addTextBody("message", textDescription);
             } catch (Exception e) {
                 logger.error("Failed to convert description", e);
                 process.complete();
--- renderer/src/main/java/me/hsgamer/mcreleaser/renderer/text/MarkdownToTextConverter.java
+++ renderer/src/main/java/me/hsgamer/mcreleaser/renderer/text/MarkdownToTextConverter.java
@@ -1,12 +1,12 @@
-package me.hsgamer.mcreleaser.html;
+package me.hsgamer.mcreleaser.renderer.text;
 
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.renderer.text.TextContentRenderer;
 
-public class MarkdownToHTMLConverter {
+public class MarkdownToTextConverter {
     private static final Parser parser = Parser.builder().build();
-    private static final HtmlRenderer renderer = HtmlRenderer.builder().build();
+    private static final TextContentRenderer renderer = TextContentRenderer.builder().build();
 
     public static String convert(String markdown) {
         Node document = parser.parse(markdown);
