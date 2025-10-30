0fbe16767a862b266b12f07b21b38a915b9e28f5
hsgamer <huynhqtienvtag@gmail.com>
2025-10-31 02:01:10 +0700

    use httpclient5 for ModrinthPlatform

--- modrinth/src/main/java/me/hsgamer/mcreleaser/modrinth/ModrinthPlatform.java
+++ modrinth/src/main/java/me/hsgamer/mcreleaser/modrinth/ModrinthPlatform.java
@@ -1,7 +1,8 @@
 package me.hsgamer.mcreleaser.modrinth;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import me.hsgamer.hscore.task.BatchRunnable;
 import me.hsgamer.hscore.task.element.TaskPool;
 import me.hsgamer.mcreleaser.core.file.FileBundle;
@@ -9,22 +10,25 @@ import me.hsgamer.mcreleaser.core.platform.Platform;
 import me.hsgamer.mcreleaser.core.property.CommonPropertyKey;
 import me.hsgamer.mcreleaser.core.util.PropertyKeyUtil;
 import me.hsgamer.mcreleaser.core.util.StringUtil;
-import me.hsgamer.mcreleaser.modrinth.api.ApiClient;
-import me.hsgamer.mcreleaser.modrinth.api.ApiResponse;
-import me.hsgamer.mcreleaser.modrinth.api.Configuration;
-import me.hsgamer.mcreleaser.modrinth.api.client.VersionsApi;
-import me.hsgamer.mcreleaser.modrinth.api.model.CreatableVersionDto;
-import me.hsgamer.mcreleaser.modrinth.api.model.VersionDependencyDto;
-import me.hsgamer.mcreleaser.modrinth.api.model.VersionDto;
 import me.hsgamer.mcreleaser.version.MinecraftVersionFetcher;
 import me.hsgamer.mcreleaser.version.VersionTypeFilter;
-import okhttp3.Call;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.lang.reflect.Type;
-import java.util.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ModrinthPlatform implements Platform {
@@ -48,21 +52,6 @@ public class ModrinthPlatform implements Platform {
         Gson gson = new Gson();
 
         BatchRunnable batchRunnable = new BatchRunnable();
-        TaskPool preparePool = batchRunnable.getTaskPool(0);
-        preparePool.addLast(process -> {
-            Configuration.setApiClientFactory(() -> {
-                ApiClient apiClient = new ApiClient();
-                apiClient.setUserAgent(CommonPropertyKey.NAME.getValue() + "/" + CommonPropertyKey.VERSION.getValue());
-                apiClient.setApiKey(ModrinthPropertyKey.TOKEN.getValue());
-                if (ModrinthPropertyKey.ENDPOINT.getValue("production").equalsIgnoreCase("staging")) {
-                    logger.info("Use staging endpoints");
-                    apiClient.setServerIndex(1);
-                }
-                return apiClient;
-            });
-            logger.info("Modrinth API is ready");
-            process.next();
-        });
 
         TaskPool requestPool = batchRunnable.getTaskPool(1);
         requestPool.addLast(process -> {
@@ -72,7 +61,7 @@ public class ModrinthPlatform implements Platform {
                 try {
                     gameVersionTypeFilter = VersionTypeFilter.valueOf(ModrinthPropertyKey.GAME_VERSION_TYPE.getValue().toUpperCase());
                 } catch (IllegalArgumentException e) {
-                    logger.error("Invalid version type: " + ModrinthPropertyKey.GAME_VERSION_TYPE.getValue(), e);
+                    logger.error("Invalid version type: {}", ModrinthPropertyKey.GAME_VERSION_TYPE.getValue(), e);
                     process.complete();
                     return;
                 }
@@ -89,106 +78,92 @@ public class ModrinthPlatform implements Platform {
             });
         });
         requestPool.addLast(process -> {
-            CreatableVersionDto dto = new CreatableVersionDto();
+            JsonObject request = new JsonObject();
 
-            dto.setProjectId(ModrinthPropertyKey.PROJECT.getValue());
-            dto.setFeatured(ModrinthPropertyKey.FEATURED.asBoolean(true));
+            request.addProperty("project_id", ModrinthPropertyKey.PROJECT.getValue());
+            request.addProperty("featured", ModrinthPropertyKey.FEATURED.asBoolean(true));
 
-            dto.setName(CommonPropertyKey.NAME.getValue());
-            dto.setChangelog(CommonPropertyKey.DESCRIPTION.getValue());
-            dto.setVersionNumber(CommonPropertyKey.VERSION.getValue());
-
-            if (ModrinthPropertyKey.VERSION_TYPE.isAbsent()) {
-                dto.setVersionType(CreatableVersionDto.VersionTypeEnum.RELEASE);
-            } else {
-                String versionTypeString = ModrinthPropertyKey.VERSION_TYPE.getValue();
-                try {
-                    dto.setVersionType(CreatableVersionDto.VersionTypeEnum.valueOf(versionTypeString.toUpperCase()));
-                } catch (IllegalArgumentException e) {
-                    logger.error("Invalid version type: " + versionTypeString, e);
-                    process.complete();
-                    return;
-                }
-            }
+            request.addProperty("name", CommonPropertyKey.NAME.getValue());
+            request.addProperty("changelog", CommonPropertyKey.DESCRIPTION.getValue());
+            request.addProperty("version_number", CommonPropertyKey.VERSION.getValue());
+            request.addProperty("version_type", ModrinthPropertyKey.VERSION_TYPE.getValue("release").toLowerCase());
 
             if (ModrinthPropertyKey.DEPENDENCIES.isPresent()) {
-                TypeToken<List<VersionDependencyDto>> typeToken = new TypeToken<>() {
-                };
                 try {
-                    List<VersionDependencyDto> dependencies = gson.fromJson(ModrinthPropertyKey.DEPENDENCIES.getValue(), typeToken.getType());
-                    dto.setDependencies(dependencies);
+                    JsonArray dependencies = gson.fromJson(ModrinthPropertyKey.DEPENDENCIES.getValue(), JsonArray.class);
+                    request.add("dependencies", dependencies);
                 } catch (Exception e) {
                     logger.error("Invalid dependencies", e);
                     process.complete();
                     return;
                 }
             } else {
-                dto.setDependencies(Collections.emptyList());
+                request.add("dependencies", gson.toJsonTree(Collections.emptyList()));
             }
 
             List<String> loaders = Arrays.asList(StringUtil.splitSpace(ModrinthPropertyKey.LOADERS.getValue()));
-            dto.setLoaders(loaders);
+            request.add("loaders", gson.toJsonTree(loaders));
             process.getData().put("loaders", loaders);
 
             List<String> gameVersions = process.getData().get("versionIds");
-            dto.setGameVersions(gameVersions);
+            request.add("game_versions", gson.toJsonTree(gameVersions));
 
             List<String> fileParts = fileBundle.allFiles().stream().map(File::getName).collect(Collectors.toList());
-            dto.setFileParts(fileParts);
+            request.add("file_parts", gson.toJsonTree(fileParts));
             if (fileBundle.primaryFile() != null) {
-                dto.setPrimaryFile(fileBundle.primaryFile().getName());
+                request.addProperty("primary_file", fileBundle.primaryFile().getName());
             }
 
-            process.getData().put("request", dto);
+            process.getData().put("request", request);
             logger.info("The request is ready");
             process.next();
         });
 
         TaskPool uploadPool = batchRunnable.getTaskPool(3);
         uploadPool.addLast(process -> {
-            VersionsApi versionsApi = new VersionsApi();
-            ApiClient apiClient = versionsApi.getApiClient();
-            CreatableVersionDto request = process.getData().get("request");
-
-            Map<String, String> headerParams = new HashMap<>();
-            final String[] accepts = {"application/json"};
-            final String accept = apiClient.selectHeaderAccept(accepts);
-            if (accept != null) {
-                headerParams.put("Accept", accept);
+            String baseUrl;
+            if (ModrinthPropertyKey.ENDPOINT.getValue("production").equalsIgnoreCase("staging")) {
+                logger.info("Use staging endpoint");
+                baseUrl = "https://staging-api.modrinth.com";
+            } else {
+                baseUrl = "https://api.modrinth.com";
             }
-            final String[] contentTypes = {"multipart/form-data"};
-            final String contentType = apiClient.selectHeaderContentType(contentTypes);
-            if (contentType != null) {
-                headerParams.put("Content-Type", contentType);
-            }
+            String url = baseUrl + "/v2/version";
+            String token = ModrinthPropertyKey.TOKEN.getValue();
+            String userAgent = CommonPropertyKey.NAME.getValue() + "/" + CommonPropertyKey.VERSION.getValue();
 
-            Map<String, Object> formParams = new HashMap<>();
-            formParams.put("data", request);
-            for (File file : fileBundle.allFiles()) {
-                formParams.put(file.getName(), file);
-            }
+            JsonObject request = process.getData().get("request");
 
-            String[] authNames = new String[]{"TokenAuth"};
-            try {
-                Call call = apiClient.buildCall(
-                        null,
-                        "/version",
-                        "POST",
-                        Collections.emptyList(),
-                        Collections.emptyList(),
-                        null,
-                        headerParams,
-                        Collections.emptyMap(),
-                        formParams,
-                        authNames,
-                        null
-                );
+            try (CloseableHttpClient client = HttpClients.createMinimal()) {
+                HttpPost post = new HttpPost(url);
+                post.setHeader("Authorization", token);
+                post.setHeader("User-Agent", userAgent);
+                post.setHeader("Accept", "application/json");
 
-                Type responseType = new TypeToken<VersionDto>() {
-                }.getType();
-                ApiResponse<VersionDto> execute = apiClient.execute(call, responseType);
-                VersionDto response = execute.getData();
-                logger.info("Uploaded the version: {}", response.getId());
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.addTextBody("data", gson.toJson(request), ContentType.APPLICATION_JSON);
+                for (File file : fileBundle.allFiles()) {
+                    try {
+                        builder.addBinaryBody(file.getName(), file);
+                    } catch (Exception e) {
+                        logger.warn("Failed to add file", e);
+                    }
+                }
+                HttpEntity entity = builder.build();
+                post.setEntity(entity);
+
+                JsonObject versionDto = client.execute(post, response -> {
+                    if (response.getCode() != 200) {
+                        throw new HttpException("Execute not successful", response.getCode(), EntityUtils.toString(response.getEntity()));
+                    }
+                    try {
+                        String body = EntityUtils.toString(response.getEntity());
+                        return gson.fromJson(body, JsonObject.class);
+                    } catch (Exception e) {
+                        throw new IOException("Failed to parse response", e);
+                    }
+                });
+                logger.info("Uploaded the version: {}", versionDto.get("id").getAsString());
             } catch (Exception e) {
                 logger.warn("Failed to upload the version", e);
             }
